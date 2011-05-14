3cdf245bd7e7a39e4d4e960f355798718aea5e0b
Kyle Wu <imkylewu@gmail.com>
2011-05-14 20:41:54 +0200

    conflict

--- client_start.py
+++ client_start.py
@@ -8,20 +8,20 @@ __version__ = '0.1'
 
 from DTNSiteManager import ClientSiteManager
 
-#ip='130.238.8.164'
+ip='130.238.8.164'
 #ip='127.0.0.1'
-ip = '130.243.144.12'
+#ip = '130.243.144.12'
 sm = ClientSiteManager(server_ip='127.0.0.1', server_port=7777, vclient_port=4445, ip=ip, sh='client')
 sm.start()
 
-sm.connect_to_sm(ip, 15555)
+#sm.connect_to_sm(ip, 15555)
 
-#while True:
-    #print 'send'
-    #sm.bcast(16666)
-    #print 'sent'
-    #import time
-    #time.sleep(5)
+while True:
+    print 'send'
+    sm.bcast(16666)
+    print 'sent'
+    import time
+    time.sleep(5)
 
 import sys
 #sys.exit(1)
